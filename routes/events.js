3aa5ed4743c7d4ff4dd60d6979dc6d1b3d81a346
Chuwei Lu <yuan329419@gmail.com>
2019-06-11 23:03:10 -0700

    Add express router OPTIONS

--- routes/events.js
+++ routes/events.js
@@ -37,7 +37,15 @@ router.get('/', cors(corsOptions), function (req, res) {
     });
 });
 
-router.post('/', function (req, res) {
+router.options("/*", function(req, res, next){
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.send(200);
+  });
+
+
+router.post('/', cors(corsOptions), function (req, res) {
     const { userId, startTime, endTime, eventName } = req.body;
     const item = { userId, startTime, endTime, eventName };
     item.userId = parseInt(req.body.userId);
